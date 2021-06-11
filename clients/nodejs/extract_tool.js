3c36b608ce658dbe589823f072c154d3ebdba727
Jose Daniel Hernandez <jsdaniel.h@gmail.com>
2021-06-11 11:56:34 -0600

    Add changes to extract tool to get additional info
    Add changes to extract tool to get additional information such as:
    - The height of the head block.
    - The timestamp of the head block.
    - Vesting contracts start as timestamp when the start is a block
      already in the blockchain.
    - HTLC contracts timeout as timestamp when the timeout is a block
      already in the blockchain.
    
    Also an additional fields to the toml to represent a custom delay
    that specifies the delta between the end of Nimiq 1.0 and the start
    of Nimiq 2.0.

--- clients/nodejs/extract_tool.js
+++ clients/nodejs/extract_tool.js
@@ -16,8 +16,8 @@ const rl = readline.createInterface({
 /**
  * @param {<Array.<AccountsTreeNode>>} accountsTreeChunk
  */
-async function dumpAccountstoToml(accountsTreeChunk, fileStream) {
-    accountsTreeChunk.forEach(function(accountTreeNode, index, array) {
+async function dumpAccountstoToml(accountsTreeChunk, chainDS, headBlockHeight, fileStream) {
+    for (const accountTreeNode of accountsTreeChunk) {
         var endOfLine = require('os').EOL;
         account = accountTreeNode.account;
         account_prefix = accountTreeNode.prefix;
@@ -42,6 +42,12 @@ async function dumpAccountstoToml(accountsTreeChunk, fileStream) {
             fileStream.write(endOfLine);
             fileStream.write(`owner = "${account_data.owner}"`);
             fileStream.write(endOfLine);
+            if (account_data.vestingStart <= headBlockHeight) {
+                vestingStartBlock = await chainDS.getBlockAt(account_data.vestingStart);
+                let vestingStartTS = vestingStartBlock.timestamp;
+                fileStream.write(`vesting_start_ts = ${vestingStartTS}`);
+                fileStream.write(endOfLine);
+            }
             fileStream.write(`vesting_start = ${account_data.vestingStart}`);
             fileStream.write(endOfLine);
             fileStream.write(`vesting_step_blocks = ${account_data.vestingStepBlocks}`);
@@ -71,6 +77,12 @@ async function dumpAccountstoToml(accountsTreeChunk, fileStream) {
             fileStream.write(endOfLine);
             fileStream.write(`hash_count = ${account_data.hashCount}`);
             fileStream.write(endOfLine);
+            if (account_data.timeout <= headBlockHeight) {
+                htlcTimeoutBlock = await chainDS.getBlockAt(account_data.timeout);
+                let htlcTimeoutTS = htlcTimeoutBlock.timestamp;
+                fileStream.write(`timeout_ts = ${htlcTimeoutTS}`);
+                fileStream.write(endOfLine);
+            }
             fileStream.write(`timeout = ${account_data.timeout}`);
             fileStream.write(endOfLine);
             fileStream.write(`total_amount = ${account_data.totalAmount}`);
@@ -78,15 +90,44 @@ async function dumpAccountstoToml(accountsTreeChunk, fileStream) {
             fileStream.write(endOfLine);
         }
 
-    })
+    }
 }
 
-async function dumpAccountsTree(fileName, chunkSize) {
+async function dump_head_block_data(chainData, fileStream, customGenesisDelay) {
+    var endOfLine = require('os').EOL;
+    let headBlockHeight = chainData.head.height;
+    let headBlockTs = chainData.head.timestamp;
+    fileStream.write("[nim_1_head_block]");
+    fileStream.write(endOfLine);
+    fileStream.write(`height = ${headBlockHeight}`);
+    fileStream.write(endOfLine);
+    fileStream.write(`timestamp = ${headBlockTs}`);
+    fileStream.write(endOfLine);
+    fileStream.write(`custom_genesis_delay = ${customGenesisDelay}`);
+    fileStream.write(endOfLine);
+    fileStream.write(endOfLine);
+    return headBlockHeight;
+}
+
+async function dumpAccountsTree(fileName, chunkSize, customGenesisDelay) {
     var fs = require('fs')
-    var FileStream = fs.createWriteStream(fileName);
+    var fileStream = fs.createWriteStream(fileName);
     const db = await Nimiq.ConsensusDB.getFull(`main-`);
     const accounts = await Nimiq.Accounts.getPersistent(db);
+    const chainDS = await Nimiq.ChainDataStore.getPersistent(db);
 
+    // Try to get the Head hash to get the chain data
+    const headHash = await chainDS.getHead();
+    if (!headHash) {
+        throw new Error(`Couldn't read from DB`);
+    }
+
+    const chainData = await chainDS.getChainData(headHash, true);
+
+    // Dump the head block info
+    const headBlockHeight = await dump_head_block_data(chainData, fileStream, customGenesisDelay);
+
+    // Dump the Accounts tree in chunks
     let actualChunksize = chunkSize;
     let startAddress = '';
 
@@ -96,11 +137,11 @@ async function dumpAccountsTree(fileName, chunkSize) {
         console.log(`Processing chunk of size: ${actualChunksize}`);
         if (actualChunksize != 0) {
             startAddress = accountsTreeChunk[accountsTreeChunk.length - 1].prefix;
-            await dumpAccountstoToml(accountsTreeChunk, FileStream);
+            await dumpAccountstoToml(accountsTreeChunk,  chainDS, headBlockHeight, fileStream);
         }
     }
 
-    FileStream.end()
+    fileStream.end();
 }
 
 function help() {
@@ -113,8 +154,9 @@ Description:
     output-file-name    TOML output file name
 
 Options:
-    --help              Display this help page
-    --batchsize SIZE    Batch size. The default is 10000.
+    --help                Display this help page
+    --genesisdelay DELAY  Custom delay for the nimiq 2.0 genesis in minutes. The default is 60 minutes.
+    --batchsize SIZE      Batch size. The default is 10000.
     `);
     rl.close();
 }
@@ -124,10 +166,11 @@ Options:
         return help();
     }
     argv.batchsize = argv.batchsize || 10000;
+    argv.genesisdelay = argv.genesisdelay || 60;
 
     try {
         outputFileName = argv._[0];
-        await dumpAccountsTree(outputFileName, argv.batchsize);
+        await dumpAccountsTree(outputFileName, argv.batchsize, argv.genesisdelay * 60);
         rl.close();
     } catch (e) {
         console.error(e.message || e.msg || e);
